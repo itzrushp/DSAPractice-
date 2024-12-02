9e2c0f82726638d6a90b9c3a00ae58b7838402ab
Rush <itzashert@gmail.com>
2024-12-02 20:27:05 +0530

    distinctElements added

--- STL/distinctElements.cpp
+++ STL/distinctElements.cpp
@@ -18,41 +18,56 @@ using namespace std;
 #define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
 #define FOR(...) F_ORC(__VA_ARGS__)(__VA_ARGS__)
 
-template<class T> void read(T& x) {
+template <class T>
+void read(T &x)
+{
     cin >> x;
 }
-template<class H, class... T> void read(H& h, T&... t) {
+template <class H, class... T>
+void read(H &h, T &...t)
+{
     read(h);
     read(t...);
 }
 
-string to_string(char c) {
+string to_string(char c)
+{
     return string(1, c);
 }
-string to_string(bool b) {
-    return b?"true":"false";
+string to_string(bool b)
+{
+    return b ? "true" : "false";
 }
-string to_string(const char* s) {
+string to_string(const char *s)
+{
     return string(s);
 }
-string to_string(string s) {
+string to_string(string s)
+{
     return s;
 }
 
-template<class T> void write(T x) {
+template <class T>
+void write(T x)
+{
     cout << to_string(x);
 }
-template<class H, class... T> void write(const H& h, const T&... t) {
+template <class H, class... T>
+void write(const H &h, const T &...t)
+{
     write(h);
     write(t...);
 }
 
-void print() {
+void print()
+{
     write("\n");
 }
-template<class H, class... T> void print(const H& h, const T&... t) {
+template <class H, class... T>
+void print(const H &h, const T &...t)
+{
     write(h);
-    if(sizeof...(t))
+    if (sizeof...(t))
         write(' ');
     print(t...);
 }
@@ -60,28 +75,36 @@ template<class H, class... T> void print(const H& h, const T&... t) {
 const int d4i[4] = {-1, 0, 1, 0}, d4j[4] = {0, 1, 0, -1};
 const int d8i[8] = {-1, -1, 0, 1, 1, 1, 0, -1}, d8j[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 
-
-
-int main() {
+int main()
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
     set<int> s;
     int q = 1;
-    int n =1;
-    cin>>n;
-    cin >> q; 
+    int n = 1;
+    cin >> n;
+    cin >> q;
 
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int a;
         cin >> a;
         s.insert(a);
     }
+    int count = 0;
 
+    while (q--)
+    {
+        int x = 0;
+        cin >> x;
 
-    while(q--) {
-       int number = 0;
-       cin>> number;
-       
+        auto it = s.lower_bound(x);
+        int count_less_than_x = distance(s.begin(), it);
+
+        auto iti = s.upper_bound(x);
+        int count_more_than_x = distance(iti, s.end());
+
+        cout << count_less_than_x << " " << count_more_than_x << "\n";
     }
 
     return 0;
